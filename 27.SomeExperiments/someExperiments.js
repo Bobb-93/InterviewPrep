1b6571cf08dde6631ae76ed63b12cf54b66f5194
Bobb-93 <borisiakimov@gmail.com>
2025-03-22 11:35:47 +0200

    add 27.SomeExperiments - IIFE

--- 27.SomeExperiments/someExperiments.js
+++ 27.SomeExperiments/someExperiments.js
@@ -4,3 +4,15 @@ console.log([]+[]);
 
 console.log('-----------');
 
+
+(function() {
+    var a = b = 5;
+    console.log(`In function ${typeof a}`);
+    
+})();
+
+console.log('Outside function: ');
+
+console.log(typeof a); 
+console.log(typeof b);
+
