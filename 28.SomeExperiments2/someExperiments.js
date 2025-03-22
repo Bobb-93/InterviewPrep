e57f442364012414bc8ed1abc761200dad5baf7d
Bobb-93 <borisiakimov@gmail.com>
2025-03-22 12:10:32 +0200

    add 28.SomeExperiments2 - micro and macro

--- 28.SomeExperiments2/someExperiments.js
+++ 28.SomeExperiments2/someExperiments.js
@@ -2,3 +2,9 @@ console.log('-----------');
 let x = 10;
 let y = (x++, x + 1, x * 2); // 9, 11, 22
 console.log(y);//undefined
+
+console.log('-----------');
+console.log('A');
+setTimeout(() => console.log('B'), 0);
+Promise.resolve().then(() => console.log('C'));
+console.log('D');
